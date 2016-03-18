fa529c43014a77897e5bc1e1154106f1d54065f0
Rob <rnice01@aurora.edu>
2016-03-18 09:07:45 -0500

    session verify quickfix
    Fixed session being verified with no parameters passed.

--- PiTectionPHPFiles/getSession.php
+++ PiTectionPHPFiles/getSession.php
@@ -2,7 +2,7 @@
 require_once('header.php');
 if(isset($_REQUEST['session'])){
 $session = $_REQUEST['session'];
-}
+
 if(isset($_REQUEST['username'])){
 $username = $_REQUEST['username'];
 }
@@ -11,19 +11,22 @@ $user = array();
 $result->data_seek(0);
 while($row =mysqli_fetch_assoc($result))
     {
-    	
+        
         $user[] = $row;
       
     } 
       if($user[0]['session'] == $session){
-    	 $user = array('status' => 'sessionOK');
-		} 
+         $user = array('status' => 'sessionOK');
+        } 
     else {
-    	$user = array('status' => 'failed');
-	}
+        $user = array('status' => 'failed');
+    }
 echo json_encode($user);
       
 mysqli_stmt_close($query);
 mysqli_close($connection);
-
+}
+else{
+echo json_encode(array('status' => 'failed'));
+}
 ?>
